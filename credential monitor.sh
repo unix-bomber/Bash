225cedde9ac837669dd3de5b25a15bb3c55b4ab5
unix-bomber <timothykaczynski@gmail.com>
2018-08-14 13:08:24 -0400

    first working cred monitor
    made edits to credential monitor, it now works

--- credential monitor.sh
+++ credential monitor.sh
@@ -1,45 +1,33 @@
 #!/bin/bash
-# -----Syslog output
-exec 1> >(logger -s -t $($timestamp basename $0)) 2>&1
 
-# -----Variables and initilization
-# -Password check/ expiration
-passwordusers=$(cat /etc/shadow | cut -d : -f1 | sed /:$/d) #list of all users
-passwordtimeepoch=$(cat /etc/shadow | cut -d : -f3 | sed /:$/d) #list of all users expire times
-# -User login expiration
-userexpireloginepoch=$(last -R -F | cut -d : -f1)
 # -When to start alerting
-reminderpassword="864000" #if password will expire in 10 days or less, create a log for splunk alert
+reminderpassword="10" #if password will expire in 10 days or less, create a log for splunk alert
 reminderlogin="1728000" #if user hasn't logged in, in this amount of days, generate alert in splunk
-
-# -----Constants
-numberofusers=${#passwordusers[@]}
 timestamp="$(date --utc +%FT%TZ)"
 epoch=$(date +%s)
-# -Convert variables to arrays
-userarray=(${passwordusers})
-passwordarray=(${passwordusers})
-userexpirearray=(${reminderlogin}) #this uses space as delimiter (${reminderlogin// / })
 
-# -----Determine password expiration & log it
-for (($i=0;i<=$length;i++)); do
-  if ($epoch - "${userexpirearray[$i]}" <= "$reminderlogin")
-    then
-      {
-      echo "${userarray[$i]} password is expiring in less than 10 days"
-      }
-  fi
+# -----Syslog output
+#exec 1> >(logger -s -t "$("$timestamp basename $0")") 2>&1
+exec 1> >(logger -s -t $(basename $0)) 2>&1 #maybe remove this?
+# ----- check for expiring passwords
+# -list of all users with a UID bigger than 1000
+passwordusers=$(awk -F ":" '$3 >= threshold'  threshold="1000" /etc/passwd | awk -F ":" '{print $1}')
+
+# -----main logic, something better needs to go here
+# -get users with expiring passwords
+for f in $passwordusers; do
+  date=$(( epoch / 86400 ))
+  passwordlastchange=$(grep "$f" /etc/shadow | cut -d: -f3)
+  passwordlife=$(grep "$f" /etc/shadow | cut -d: -f5)
+  passwordexpire=$(( passwordlastchange + passwordlife - date))
+        if (( "$passwordexpire" <= "$reminderpassword" )); then
+        echo "User $f on $HOSTNAME has a password expiring in less than $reminderpassword days"
+        fi
+# -get users with accounts about to "idle out"
+  lastuserlogin=$(last -R -F | grep -m 1 "$f" | awk '{$2=$2};1' | cut -d ' ' -f4,5,6,7)
+  lastuserloginepoch=$(date -d "$lastuserlogin" +%s)
+  idleout=$(( lastuserloginepoch - epoch ))
+        if (( "$idleout" >= "$reminderlogin" )); then
+        echo "User $f on $HOSTNAME hasn't logged in, in 20 days. They have $lastuserloginepoch hours to do so"
+        fi
 done
-
-# -----Determine if user hasn't logged in, in 'x' days
-for ($f in $lastuserloginepoch); do
-  if ($lastlogin - "$f" < "$reminderlogin")
-
-  for (($i=0;i<=$length;i++)); do
-    if ($epoch - "${userarray[$i]}" < "$reminder")
-      then
-        {
-        echo "${userarray[$i]} has to login in less than 10 days"
-        }
-    fi
-  done
